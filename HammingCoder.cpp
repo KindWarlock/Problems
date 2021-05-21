e58cb5f1228aca939bf0a2caa483722b802a0b56
KindWarlock <71859071+KindWarlock@users.noreply.github.com>
2021-05-22 00:19:31 +0800

    Add files via upload

---
+++ HammingCoder.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+void to_bits(int num) {
+    for (int i = 15; i >= 0; i--)
+        cout << ((num >> i) & 1);
+    cout << endl;
+}
+
+void to_byte(int& num) {
+    //  контр. биты будут располагаться на поз. 4, 2, 1 
+    bool zero_bit = (num & 1);
+    num = num << 4;
+    num = num & 0b11100000;
+    num += (zero_bit << 3);
+}
+
+void contr_sums(int& num) {
+    to_byte(num);
+    bool cs1, cs2, cs3;
+    bool bits[8];
+    for (int i = 0; i < 8; i++)
+        bits[i] = (num >> i) & 1;
+    cs1 = bits[3] ^ bits[5] ^ bits[7];
+    cs2 = bits[3] ^ bits[6] ^ bits[7];
+    cs3 = bits[5] ^ bits[6] ^ bits[7];
+    num |= (cs1 << 1) | (cs2 << 2) | (cs3 << 4);
+}
+
+
+int main()
+{
+    int num, num1, num2;
+    cin >> num;
+    num1 = num >> 4;
+    num2 = num ^ (num1 << 8);
+    contr_sums(num1);
+    contr_sums(num2);
+    int coded_num = (num1 << 8) | num2;
+    cout << "Decimal: " << coded_num << endl;
+    cout << "Binary: ";
+    to_bits(coded_num);
+    return 0;
+}
+
---
+++ HammingDecoder.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+using namespace std;
+
+void to_bits(int num) {
+    for (int i = 15; i >= 0; i--) 
+        cout << ((num >> i) & 1);
+    cout << endl;
+}
+
+void check_cs(int &num) {
+    int ch = 0;     // здесь будут храниться ошибки
+    bool cs_cnt[3], bits[8];
+    for (int i = 0; i < 8; i++)
+        bits[i] = (num >> i) & 1;  
+    cs_cnt[0] = bits[1] ^ bits[3] ^ bits[5] ^ bits[7];
+    cs_cnt[1] = bits[2] ^ bits[3] ^ bits[6] ^ bits[7];
+    cs_cnt[2] = bits[4] ^ bits[5] ^ bits[6] ^ bits[7];
+    for (int i = 0; i < 3; i++) {
+        if (cs_cnt[i] == 1)
+            ch |= (1 << i);
+    }
+    bits[ch] = ~bits[ch] & 1;   // инвертируем, берем нулевой (наш) бит
+    num &= 0;
+    for (int i = 0; i < 8; i++)
+        num |= bits[i] << i;
+}
+
+int to_true(int num) {
+    bool zero_bit = num & 0b00001000;  // наш нулевой бит, который сейчас второй
+    num = (num & 0b11100000 >> 4) | zero_bit;
+    return num;
+}
+
+
+
+int main()
+{
+    int coded_num = 0, num1, num2, true_num;
+    int ch;
+    cout << "To enter a decimal num enter 1, to enter a binary num enter 2, to exit enter -1: ";
+    cin >> ch;
+    while (ch != -1){
+        if (ch == 2) {
+            char bin_num[16];
+            cin >> bin_num;
+            for (int i = 0; i < 16; i++) {
+                coded_num |= (bin_num[i] - '0') << (15 - i);    // порядок инвертитровался
+            }
+        }
+        else 
+            cin >> coded_num;
+        num1 = coded_num >> 8;
+        num2 = coded_num ^ (num1 << 8);
+        check_cs(num1);
+        check_cs(num2);
+        true_num = (to_true(num1) << 4) | to_true(num2);
+        cout << "Decimal: " << true_num << endl;
+        cout << "Binary: ";
+        to_bits(true_num);
+        cout << "To enter a decimal num enter 1, to enter a binary num enter 2, to exit enter -1: ";
+        cin >> ch;
+    }
+    return 0;
+}
