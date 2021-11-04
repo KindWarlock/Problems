c913fb738d9af84658486c83400f8493965512ce
KindWarlock <71859071+KindWarlock@users.noreply.github.com>
2021-11-04 21:38:51 +0800

    Add files via upload

---
+++ Vector/Array.cpp
@@ -0,0 +1,109 @@
+#include "Array.h"
+
+Array::Array(int startSize) {
+	if (startSize <= 0)
+		capacity = DEFAULT_SIZE;
+	else
+		capacity = startSize;
+	ptr = new int[capacity];
+	size = 0;
+}
+
+Array::Array(const Array& arr)
+{
+	ptr = new int[arr.capacity];
+	size = arr.size;
+	capacity = arr.capacity;
+	for (int i = 0; i < size; i++) {
+		ptr[i] = arr.ptr[i];
+	}
+}
+
+Array::~Array()
+{
+	delete[] ptr;
+}
+
+Array& Array::operator= (const Array& arr) {
+	if (size < arr.size) {
+		delete[] ptr;
+		if (capacity < arr.capacity) {
+			capacity = arr.capacity;
+		}
+		ptr = new int[capacity];
+	}
+	size = arr.size;
+	for (int i = 0; i < size; i++) {
+		ptr[i] = arr.ptr[i];
+	}
+	return(*this);
+}
+
+int& Array::operator[](int index)
+{
+	try {
+		if (index >= size || index < 0) {
+			throw out_of_range("wrong index");
+		}
+		return ptr[index];
+	}
+	catch (out_of_range ex) {
+		cout << ex.what() << endl;
+	}
+}
+
+void Array::expand(int diffCap)
+{
+	int* tmp = new int[capacity + diffCap];
+	for (int i = 0; i < capacity; i++) {
+		tmp[i] = ptr[i];
+	}
+	delete[] ptr;
+	ptr = tmp;
+	capacity += diffCap;
+}
+
+void Array::insert(int num, int index) {
+	try {
+		if (index > size || index < 0) {
+			throw out_of_range("wrong index");
+		}
+
+		if (size == capacity)
+			expand();
+		ptr[index] = num;
+		size++;
+	}
+	catch (out_of_range ex) {
+		cout << ex.what() << endl;
+	}
+}
+
+void Array::insert(int num) {
+	insert(num, size);
+}
+
+void Array::remove(int index)
+{
+	try {
+		if (index > size || index < 0) {
+			throw out_of_range("wrong index");
+		}
+
+		/* при i == size - 1 ничего плохого не произойдет,
+		т.к доступа в ту €чейку не будет */
+		for (int i = index; i < size; i++) {
+			ptr[i] = ptr[i + 1];	
+		}
+		size--;
+	}
+	catch (out_of_range ex) {
+		cout << ex.what() << endl;
+	}
+}
+
+
+int Array::getSize()
+{
+	return size;
+}
---
+++ Vector/Vector.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include "Array.h"
+
+using namespace std;
+
+int main()
+{
+	Array vec;
+	//инициализация вектора с превыщением capacity по умолчанию
+	for (int i = 0; i <= 33; i++) {
+		vec.insert(i);
+		cout << vec[i] << ' ';
+	}
+	cout << endl;
+	
+	//удаление элемента
+	vec.remove(30);
+	for (int i = 0; i < vec.getSize(); i++) {
+		cout << vec[i] << ' ';
+	}
+	cout << endl;
+
+	//конструктор копирования
+	Array vec_new(vec);
+	vec_new.insert(vec_new.getSize());
+	for (int i = 0; i < vec_new.getSize(); i++) {
+		cout << vec_new[i] << ' ';
+	}
+	cout << endl;
+
+	//копирование
+	vec_new = vec;
+	for (int i = 0; i < vec_new.getSize(); i++) {
+		cout << vec_new[i] << ' ';
+	}
+	return 0;
+}
