867941f7c840580c935e17854d10a09b68684748
KindWarlock <71859071+KindWarlock@users.noreply.github.com>
2021-11-04 21:38:28 +0800

    Create Array.h

---
+++ Vector/Array.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+const int DEFAULT_SIZE = 32;
+const int EXPAND_CAPACITY = 10;
+
+class Array
+{
+	int *ptr;
+	int size;
+	int capacity;
+
+public:
+	explicit Array(int startSize=DEFAULT_SIZE);
+	~Array();
+	Array(const Array &arr);
+
+	Array& operator= (const Array &arr);
+	int& operator[] (int index);
+
+	
+	void expand(int diffCap=EXPAND_CAPACITY);
+	void insert(int num);
+	void insert(int num, int index);
+	
+	int getSize();
+	void remove(int index);
+};
+
