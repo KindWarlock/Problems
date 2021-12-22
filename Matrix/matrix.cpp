58a80a9ff1af47765137c32cf9d0e69b3cf29a43
KindWarlock <71859071+KindWarlock@users.noreply.github.com>
2021-12-22 21:22:35 +0800

    Update matrix.cpp

--- Matrix/matrix.cpp
+++ Matrix/matrix.cpp
@@ -1,13 +1,13 @@
-#include "matrix.h"
-
+#include "Matrix.h"
 Matrix::Matrix(int rows_num, int cols_num) {
-    *rows = new Vector<Vector<int>>(rows_num);
+    rows = new Vector<Vector<int>>(rows_num);
     for (int i = 0; i < rows_num; i++) {
         Vector<int> row(cols_num);
-        rows[i] = row;
+        rows->insert(row, i);
     }
 }
 
-Matrix::~Matrix() {
-    delete *rows;
+const Vector<int>& Matrix::operator[](int index)
+{
+    return rows[index];
 }
