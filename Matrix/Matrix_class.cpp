a804f3fa1914b0baf988c1c4e69fb96108be510c
KindWarlock <71859071+KindWarlock@users.noreply.github.com>
2022-01-17 19:09:10 +0800

    Update Matrix_class.cpp

--- Matrix/Matrix_class.cpp
+++ Matrix/Matrix_class.cpp
@@ -1,28 +1,73 @@
 #include <iostream>
 #include "Matrix.h"
-
+#include "cmath"
 using namespace std;
 
+void fill_matrix(Matrix& m) {
+	double cnt = 0.0;
+	for (int i = 0; i < m.getRowsNum(); i++) {
+		for (int k = 0; k < m.getColumnsNum(); k++) {
+			m[i][k] = cnt++;
+		}
+	}
+	m[1][2] = 2;
+}
+
 int main() {
-    Matrix test_m(3, 3);
-	cout << test_m << endl;
+	//создание матрицы
+	Matrix m(3, 3);
+	fill_matrix(m);
+	cout << m << endl;
 
-	Vector<int> test_col(3);
-	int rat[3] = {1,1,1};
-	test_col.insert(rat, 3);
+	//добавление ряда
+	Vector<double> row(3);
+	for (int i = 0; i < 3; i++) {
+		row.insert(i);
+	}
+	m.insertRow(row);
+	cout << m << endl;
 
-	Vector<int> test_row(4);
-	int cat[4] = {4,4,4,4};
-	test_row.insert(cat, 4);
-
-	test_m.insertColumn(test_col);
-	cout << test_m<< endl;
-	test_m.insertRow(test_row);
-	cout << test_m << endl;
+	//добавление столбца
+	Vector<double> col(4);
+	for (int i = 0; i < 4; i++) {
+		col.insert(i);
+	}
+	m.insertColumn(col);
+	cout << m << endl;
 	
-	cout << "transposed: " << endl;
-	Matrix tr(test_m.transpose());
-	cout << tr;
-	system("pause");
-    return 0;
+	//поиск определителя
+	cout << "Determinant: " << m.findDeterminant() << "\n\n";
+
+	//присвоение матрицы и обратная матрица
+	Matrix temp_m = m.reverse();
+	cout << "Reversed:\n" << temp_m << endl;
+
+	//проверка обратной матрицы и умножение
+	temp_m = temp_m * m;
+	cout << "Identity matrix:\n" << temp_m << endl;
+
+	//деление матрицы на саму себя:
+	temp_m = m / m;
+	cout << "Divided:\n" << temp_m << endl;
+	
+	//транспонирование:
+	temp_m = m.transpose();
+	cout << "Transposed:\n" << temp_m << endl;
+
+	//вычитание и сложение изначальной 4х4 и транспонированной
+	temp_m = m - temp_m;
+	cout << "Subtracted:\n" << temp_m << endl;
+	temp_m = m - temp_m;
+	temp_m = m + temp_m;
+	cout << "Added:\n" << temp_m << endl;
+
+	//умножение на число
+	temp_m = m * 2.0;
+	cout << "Multiply by number (2):\n" << temp_m << endl;
+
+	//нахождение минора
+	temp_m = m.findMinor(0,2);
+	cout << "Minor (0, 2):\n" << temp_m << endl;
+
+	return 0;
 }
